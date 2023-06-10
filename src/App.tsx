be80b094022b5c2edf089efb7e9ab817b3c41316
Andriikryl <andreyk557@gmail.com>
2023-06-10 20:19:09 +0300

    add random color-box

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [color, setColor] = useState("");
+
+  const getRandomColor = () => {
+    const digits = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+    ];
+    const color = new Array(6)
+      .fill("")
+      .map(() => digits[Math.floor(Math.random() * digits.length)])
+      .join("");
+    return `#${color}`;
+  };
+
+  useEffect(() => {
+    setColor(getRandomColor());
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="container" style={{ background: color }}></div>
+    </div>
+  );
 }
 
 export default App;
