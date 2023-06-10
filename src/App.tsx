30e5587570f3c0013c3de86fcf3bc026e9545f21
Andriikryl <andreyk557@gmail.com>
2023-06-10 21:47:38 +0300

    add settimout functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,45 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+const getRandomColor = () => {
+  const digits = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+  ];
+  const color = new Array(6)
+    .fill("")
+    .map(() => digits[Math.floor(Math.random() * digits.length)])
+    .join("");
+  return `#${color}`;
+};
+
+enum Resul {
+  Correct,
+  Wrong,
+}
+
 function App() {
   const [color, setColor] = useState("");
   const [answers, setAnswers] = useState<string[]>([]);
-  const [isWrongSelection, setIsWrongSelection] = useState(false);
+  const [isWrongSelection, setIsWrongSelection] = useState<Resul | undefined>(
+    undefined
+  );
 
-  const getRandomColor = () => {
-    const digits = [
-      "0",
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "A",
-      "B",
-      "C",
-      "D",
-      "E",
-      "F",
-    ];
-    const color = new Array(6)
-      .fill("")
-      .map(() => digits[Math.floor(Math.random() * digits.length)])
-      .join("");
-    return `#${color}`;
-  };
-
-  useEffect(() => {
+  const pickColor = () => {
     const actualColor = getRandomColor();
     setColor(actualColor);
     setAnswers(
@@ -40,26 +47,41 @@ function App() {
         () => 0.5 - Math.random()
       )
     );
+  };
+
+  useEffect(() => {
+    pickColor();
   }, []);
 
-  function handleAnswerClicked(answer: string) {
+  const handleAnswerClicked = (answer: string) => {
     if (answer === color) {
-      setIsWrongSelection(false);
+      setIsWrongSelection(Resul.Correct);
+      setTimeout(() => {
+        pickColor();
+      }, 1000);
+      setTimeout(() => {
+        setIsWrongSelection(undefined);
+      }, 1000);
     } else {
-      setIsWrongSelection(true);
+      setIsWrongSelection(Resul.Wrong);
     }
-  }
+  };
 
   return (
     <div className="App">
       <div className="answer-box">
-        {isWrongSelection && <span className="answer-text">Wrong Answer</span>}
+        {isWrongSelection === Resul.Wrong && (
+          <span className="answer-text">Wrong Answer</span>
+        )}
+        {isWrongSelection === Resul.Correct && (
+          <span className="answer-text | correct">Correct Answer</span>
+        )}
       </div>
       <div className="container" style={{ background: color }}></div>
       <div className="btn-box">
         {answers.map((answer) => (
           <>
-            <button onClick={() => handleAnswerClicked(answer)}>
+            <button onClick={() => handleAnswerClicked(answer)} key={answer}>
               {answer}
             </button>
           </>
